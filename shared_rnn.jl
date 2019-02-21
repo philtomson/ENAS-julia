24b30d9fe80dde0863079c789ed25cc76ea144c6
Phil Tomson <philtomson@gmail.com>
2019-02-21 06:38:42 -0800

    added RNN

--- shared_rnn.jl
+++ shared_rnn.jl
@@ -1,3 +1,14 @@
+using Random
+using ArgParse
+using Statistics
+using Flux
+using DataStructures
+using Zygote
+
+shared_embed = 1000
+shared_hid   = 1000
+BATCH_SIZE   = 64
+NUM_BLOCKS   = 12
 mutable struct Node
    id::Integer
    sigma::Function
@@ -27,16 +38,18 @@ end
 mutable struct ChildRNN
    w_xc #weights x->c
    w_xh #weights x->h
-   w_c  #default dict
-   w_h  #default dict
+   w_c::DefaultDict  #default dict
+   w_h::DefaultDict  #default dict
 end
 
-function ChildRNN(shared_embed, shared_hid, batch_size, init=randn) 
+function ChildRNN(shared_embed, shared_hid, batch_size, num_blocks=NUM_BLOCKS,init=randn) 
    w_c = DefaultDict(Dict)
    w_h = DefaultDict(Dict)
-   init_wdict([w_c,w_h],numblocks,shared_hid)
+   init_wdict([w_c,w_h],num_blocks,shared_hid)
    ChildRNN(Linear(shared_embed,shared_hid),
             Linear(shared_embed,shared_hid),
             w_c,
             w_h)
 end
+
+rnn = ChildRNN(shared_embed, shared_hid, BATCH_SIZE)
